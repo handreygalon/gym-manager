e470bc2abf17928569f49b1f1ba51b49d6f55760
handreygalon <handrey.eg@gmail.com>
2020-10-06 07:33:47 -0300

    # Initial configuration and application of CSS transitions

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const nunjuncks = require('nunjucks')
+const routes = require('./routes')
 
 const server = express()
-const videos = require("./data")
 
 server.use(express.static('public'))
+server.use(routes)
 
 server.set("view engine", "njk")
 
@@ -13,53 +14,6 @@ nunjuncks.configure("views", {
     autoescape: false,
     noCache: true
 })
-
-server.get("/", function(req, res) {
-    const about = {
-        avatar: "https://avatars1.githubusercontent.com/u/19215767?s=460&u=160c2ec1907118fa09342f84a814fcf7e8c91828&v=4",
-        name: "Handrey Emanuel Galon",
-        role: "Student - Rocketseat",
-        description: '<a href="https://rocketseat.com.br" target="_blank">Rocketseat</a> launchbase bootcamp student aiming to fit into the market as a full-stack developer.',
-        links: [
-            { name: "Github", url: "https://github.com/handreygalon" },
-            { name: "LinkedIn", url: "https://www.linkedin.com/in/handrey-emanuel-galon" },
-            { name: "Facebook", url: "https://www.facebook.com/handrey.emanuel/" }
-        ] 
-    }
-
-    return res.render("about", { about })
-})
-
-server.get("/portfolio", function(req, res) {
-    return res.render("portfolio", { items: videos })
-})
-
-server.get("/video", function(req, res) {
-    const id = req.query.id
-    const video = videos.find(function(video) {
-        return video.id == id
-    })
-
-    if (!video) {
-        return res.send("Video not found!")
-    }
-    
-    return res.render("video", { item: video })
-})
-
-/*server.get("/video/:id", function(req, res) {
-    const id = req.params.id;
-    //return res.send(`O id fornecido na rota é: ${id}`);
-    const video = videos.find(function(video) {
-        return video.id == id.split('=')[1]
-    })
-
-    if (!video) {
-        return res.send("Video not found!")
-    }
-    
-    return res.render("video", { item: video })
-})*/
 
 server.use(function(req, res) {
     res.status(404).render("not-found");
