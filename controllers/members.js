a345f0341946113b0f13ab82e9a06596724fe020
handreygalon <handrey.eg@gmail.com>
2021-02-24 19:16:41 -0300

    # New member - POST

--- controllers/members.js
+++ controllers/members.js
@@ -42,21 +42,18 @@ exports.post = function(req, res) {
             return res.send("Please, fill all the fields")
     }
 
-    let {avatar_url, birth, name, services, gender} = req.body
-    console.log(req.body)
+    birth = Date.parse(req.body.birth)
 
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)    
+    let id = 1
+    const lastMember = data.members[data.members.length - 1]
+    if (lastMember) {
+        id = lastMember.id + 1
+    }
 
     data.members.push({
         id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        services,
-        created_at
+        ...req.body,
+        birth
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
